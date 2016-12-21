bf36da5064a12fa7393d0973766ecfa835e263ed
kolberg <kolbergconsulting@gmail.com>
2016-12-21 10:45:41 -0500

    dont try injecting git user

--- jobs/jenkins_pipeline_dev.groovy
+++ jobs/jenkins_pipeline_dev.groovy
@@ -1,16 +1,13 @@
 stage('fetch') {
     node {
-        git credentialsId: '$GIT_USER', url: '$SSH_REPO'
-        stash name: '$NAME'
+        git credentialsId: 'jenkins-git', url: '$SSH_REPO'
     }
 }
 stage('build') {
     node {
-        unstash '$NAME'
         sh '''npm run globals'''
         sh '''npm run sonar'''
         sh '''sonar-runner'''
-        stash '$NAME'
         junit 'testreports/*jenkins.xml'
     }
 }
@@ -19,8 +16,7 @@ stage('deploy') {
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'), 
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-            unstash '$NAME'
-            sh '''npm run deploy -- --stage dev'''
+            sh '''serverless deploy --stage dev'''
         }
     }
 }
