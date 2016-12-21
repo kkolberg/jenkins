3d484b596b89acaa890bbfc85e78f07eb24e7088
kolberg <kolbergconsulting@gmail.com>
2016-12-21 11:02:52 -0500

    updating prod job

--- jobs/jenkins_pipeline_prod.groovy
+++ jobs/jenkins_pipeline_prod.groovy
@@ -1,32 +1,23 @@
-stage 'fetch'
+stage('fetch') {
     node {
-        git credentialsId: 'jenkins', url: 'git@github.com:kkolberg/prototypes.git'
+        git credentialsId: 'jenkins-git', url: '$SSH_REPO'
         sh '''git checkout tags/$TAG'''
-        stash name: 'starter'
     }
-stage 'build'
+}
+stage('build and test') {
     node {
-        unstash 'starter'
-        dir('starter') {
-            sh '''npm install'''
-            sh '''npm run jenkins'''
-        }
-        stash 'starter'
+        sh '''npm run jenkins'''
         junit 'starter/testreports/*.xml'
     }
-stage 'deploy'
+}
+stage('deploy') {
     node {
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'), 
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                withEnv(['NODE_ENV=production', 'IS_NOT_LOCAL=true']) {
-                    unstash 'starter'
-                    dir('starter') {
-                        sh '''npm prune'''
-                        sh '''npm version major'''
-                        sh '''serverless deploy --stage prod'''
-                    }
-                }
-            stash 'starter'
+            withEnv(['NODE_ENV=production', 'IS_NOT_LOCAL=true']) {
+                sh '''npm run deploy -- --stage prod'''
+            }
         }
     }
+}
