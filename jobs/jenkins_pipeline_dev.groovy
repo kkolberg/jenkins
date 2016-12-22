85ffa7b56a65ae1c12666fe1cc05a8b2bce5259b
kolberg <kolbergconsulting@gmail.com>
2016-12-21 22:35:47 -0500

    newman

--- jobs/jenkins_pipeline_dev.groovy
+++ jobs/jenkins_pipeline_dev.groovy
@@ -31,6 +31,7 @@ stage('deploy') {
 }
 stage('integration tests') {
     node {
+        sh '''npm install -g newman'''
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'), 
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
