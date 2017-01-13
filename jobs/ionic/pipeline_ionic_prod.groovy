8dcfa9ca0ef1357e37db1da5699dbb3062176555
kolberg <kolbergconsulting@gmail.com>
2017-01-13 16:41:50 -0500

    output

--- jobs/ionic/pipeline_ionic_prod.groovy
+++ jobs/ionic/pipeline_ionic_prod.groovy
@@ -4,6 +4,7 @@ stage('deploy') {
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
                 def output = sh returnStdout: true, script: "aws s3 ls ${BUCKET}/stage/${TAG} | wc -l"
+                echo output
                 if(output == "1"){
                     sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
                 }else{
