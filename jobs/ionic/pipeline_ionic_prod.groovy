11875ef03f4d3a39fd1e8407f21202f527d09f0b
kolberg <kolbergconsulting@gmail.com>
2017-01-13 16:40:35 -0500

    test if stage exists

--- jobs/ionic/pipeline_ionic_prod.groovy
+++ jobs/ionic/pipeline_ionic_prod.groovy
@@ -3,7 +3,12 @@ stage('deploy') {
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
+                def output = sh returnStdout: true, script: "aws s3 ls ${BUCKET}/stage/${TAG} | wc -l"
+                if(output == "1"){
+                    sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
+                }else{
+                    currentBuild.result = 'FAILURE'
+                }
             }
     }
 }
