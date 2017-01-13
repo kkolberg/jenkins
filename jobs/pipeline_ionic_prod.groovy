08b2e0d3618f4434cc8c2a7390deaab1ac57eea5
kolberg <kolbergconsulting@gmail.com>
2017-01-13 14:23:10 -0500

    staging vs prod

--- jobs/pipeline_ionic_prod.groovy
+++ jobs/pipeline_ionic_prod.groovy
@@ -1,30 +1,9 @@
-stage('fetch') {
-    node {
-         git branch: 'live', credentialsId: 'jenkins-git', url: '$SSH_REPO'
-        sh '''git checkout tags/$TAG'''
-    }
-}
-stage('build') {
-    node {
-        sh '''npm run globals'''
-        sh '''npm install'''
-        sh '''npm run ionic:build'''
-    }
-}
-stage('code quality') {
-    node {
-        // sh '''npm run sonar'''
-        // sh '''sonar-runner'''
-        // junit 'testreports/*jenkins.xml'
-    }
-}
 stage('deploy') {
-    //TO-DO!  When prod credentials are created.  Go into Jenkins > Credentials, add new secret text and then replace AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to the contstant named in the new credentials
     node {
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                sh '''aws s3 sync ./www s3://$BUCKET/stage/$TAG --delete'''
+                sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
             }
     }
 }
--- jobs/pipeline_live_prod.groovy
+++
@@ -1,9 +0,0 @@
-stage('deploy') {
-    node {
-        withCredentials([
-            string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
-            string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
-            }
-    }
-}
