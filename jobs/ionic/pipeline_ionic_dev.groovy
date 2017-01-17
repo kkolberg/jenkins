7ee7ae5adac324d683850c9cbcf1ca346e739887
Scott Hankinson <scott.hankinson@slalom.com>
2017-01-17 15:02:11 -0500

    Updating pipeline for ENV

--- jobs/ionic/pipeline_ionic_dev.groovy
+++ jobs/ionic/pipeline_ionic_dev.groovy
@@ -28,6 +28,10 @@ stage('update environment'){
         sh "rm ./www/assets/json/student-resources.qa.json"
         sh "rm ./www/assets/json/student-resources.prod.json"
         sh "mv ./www/assets/json/student-resources.dev.json ./www/assets/json/student-resources.json"
+        sh "rm ./www/assets/json/env.json"
+        sh "rm ./www/assets/json/env.qa.json"
+        sh "rm ./www/assets/json/env.prod.json"
+        sh "mv ./www/assets/json/env.dev.json ./www/assets/json/env.json"
     }
 }
 stage('deploy') {
