0872d929414d5b406eb64c5f045647fabc3eaddf
Scott Hankinson <scott.hankinson@slalom.com>
2017-01-19 14:12:14 -0500

    Tweaking to use separate Stage and Deploy buckets

--- jobs/ionic/pipeline_ionic_prod.groovy
+++ jobs/ionic/pipeline_ionic_prod.groovy
@@ -1,12 +1,12 @@
 stage('deploy') {
     node {
         withCredentials([
-            string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
-            string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                def output = sh returnStdout: true, script: "aws s3 ls ${BUCKET}/stage/${TAG} | wc -l"
+            string(credentialsId: 'PROD_AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
+            string(credentialsId: 'PROD_AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
+                def output = sh returnStdout: true, script: "aws s3 ls ${STAGE_BUCKET}/${TAG} | wc -l"
                 echo output
-                if(output as Integer  == 1){
-                    sh '''aws s3 sync s3://$BUCKET/stage/$TAG s3://$BUCKET/live --delete'''
+                if(output as Integer == 1){
+                    sh '''aws s3 sync s3://$STAGE_BUCKET/$TAG/ s3://$BUCKET/ --delete'''
                 }else{
                     currentBuild.result = 'FAILURE'
                 }
