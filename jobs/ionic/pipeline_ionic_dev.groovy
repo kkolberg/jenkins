73281af1f10a7a768b9e7e91e6cc46c43e721eec
Scott Hankinson <scott.hankinson@slalom.com>
2017-01-30 15:36:17 -0500

    Updating to use Browser platform build

--- jobs/ionic/pipeline_ionic_dev.groovy
+++ jobs/ionic/pipeline_ionic_dev.groovy
@@ -13,10 +13,6 @@ stage('build') {
         sh "npm install"
         sh "npm run ionic:build"
         sh "cordova platform add browser"
-        sh "rm -R ./www"
-        sh "ls"
-        sh "ls ./platforms"
-        sh "cp -R ./platforms/browser/www ./www"
     }
 }
 stage('code quality') {
@@ -29,14 +25,14 @@ stage('code quality') {
 stage('update environment'){
     node {
         echo '@@@@ Setting Environment Variables File for DEV @@@@'
-        sh "rm ./www/assets/json/student-resources.json"
-        sh "rm ./www/assets/json/student-resources.qa.json"
-        sh "rm ./www/assets/json/student-resources.prod.json"
-        sh "mv ./www/assets/json/student-resources.dev.json ./www/assets/json/student-resources.json"
-        sh "rm ./www/assets/json/env.json"
-        sh "rm ./www/assets/json/env.qa.json"
-        sh "rm ./www/assets/json/env.prod.json"
-        sh "mv ./www/assets/json/env.dev.json ./www/assets/json/env.json"
+        sh "rm ./platforms/browser/www/assets/json/student-resources.json"
+        sh "rm ./platforms/browser/www/assets/json/student-resources.qa.json"
+        sh "rm ./platforms/browser/www/assets/json/student-resources.prod.json"
+        sh "mv ./platforms/browser/www/assets/json/student-resources.dev.json ./platforms/browser/www/assets/json/student-resources.json"
+        sh "rm ./platforms/browser/www/assets/json/env.json"
+        sh "rm ./platforms/browser/www/assets/json/env.qa.json"
+        sh "rm ./platforms/browser/www/assets/json/env.prod.json"
+        sh "mv ./platforms/browser/www/assets/json/env.dev.json ./platforms/browser/www/assets/json/env.json"
     }
 }
 stage('deploy') {
@@ -44,7 +40,7 @@ stage('deploy') {
         withCredentials([
             string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
             string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-                sh '''aws s3 sync ./www s3://$BUCKET --delete'''
+                sh '''aws s3 sync ./platforms/browser/www s3://$BUCKET --delete'''
             }
     }
 }
