2b016a1c320c8d750921d6bd53751ebb9db52452
Scott Hankinson <scott.hankinson@slalom.com>
2017-01-30 15:08:32 -0500

    Update pipeline_ionic_dev.groovy

--- jobs/ionic/pipeline_ionic_dev.groovy
+++ jobs/ionic/pipeline_ionic_dev.groovy
@@ -9,11 +9,11 @@ stage('fetch') {
 }
 stage('build') {
     node {
-        sh '''npm run globals'''
-        sh '''npm install'''
-        sh '''npm run ionic:'build browser''
-        sh '''rm -R ./www'''
-        sh '''cp -R ./platforms/browser/www ./www'''
+        sh "npm run globals"
+        sh "npm install"
+        sh "npm run ionic:build browser"
+        sh "rm -R ./www"
+        sh "cp -R ./platforms/browser/www ./www"
     }
 }
 stage('code quality') {
